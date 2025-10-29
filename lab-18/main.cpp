1a88b550b89c56e2221f873551973b08953ac37e
Liam Walker <liamwalker@liamwalker.me>
2025-10-28 19:47:04 -0700

    added labs 18 and 19

---
+++ lab-18/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+void treeTop();
+void treeTrunk(int height);
+void drawTrees(int numberOfTrees, int heightOfTrees);
+
+int main()
+{
+  int numberOfTrees = 0;
+  int heightOfTrees = 0;
+
+  cout << "Enter tree height: ";
+  cin >> heightOfTrees;
+
+  cout << "Enter number of trees: ";
+  cin >> numberOfTrees;
+
+  drawTrees(numberOfTrees, heightOfTrees);
+
+}
+
+
+void treeTop() {
+  cout << "   ^   " << endl;
+  cout << "  / \\  " << endl;
+  cout << " /   \\ " << endl;
+  cout << "/_____\\ " << endl;
+}
+
+void treeTrunk(int height) {
+  for (int i = 0; i < height; ++i) {
+    cout << "  | |   " << endl;
+  }
+}
+
+void drawTrees(int numberOfTrees, int heightOfTrees) {
+  for (int i = 0; i < numberOfTrees; ++i) {
+    treeTop();
+    treeTrunk(heightOfTrees);
+    cout << endl;
+  }
+}
---
+++ lab-19/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+void treeTop();
+void treeTrunk(int height);
+void drawTrees(int numberOfTrees, int heightOfTrees);
+
+int main()
+{
+  char repeat;
+
+  do {
+
+    int numberOfTrees = 0;
+    int heightOfTrees = 0;
+
+    cout << "Enter tree height: ";
+    cin >> heightOfTrees;
+
+    cout << "Enter number of trees: ";
+    cin >> numberOfTrees;
+
+    drawTrees(numberOfTrees, heightOfTrees);
+
+    
+
+    cout << "Do you have another entry?: ";
+    cin >> repeat;
+
+
+
+  } while (repeat == 'Y' || repeat == 'y');
+  
+
+}
+
+void treeTop() {
+  cout << "   ^   " << endl;
+  cout << "  / \\  " << endl;
+  cout << " /   \\ " << endl;
+  cout << "/_____\\ " << endl;
+}
+
+void treeTrunk(int height) {
+  for (int i = 0; i < height; ++i) {
+    cout << "  | |   " << endl;
+  }
+}
+
+void drawTrees(int numberOfTrees, int heightOfTrees) {
+  static int count = 0;
+  count += numberOfTrees;
+
+  for (int i = 0; i < numberOfTrees; ++i) {
+    treeTop();
+    treeTrunk(heightOfTrees);
+    cout << endl;
+  }
+
+  cout << "There are currently " << count << " trees in your forest." << endl;
+}
